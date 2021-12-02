cf86fa9d7dfb92c5531850e9c858cb2f6fba268a
dusty-turner <dusty.s.turner.mil@mail.mil>
2021-12-02 10:15:21 -0500

    day 2

---
+++ day_2.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+## 1
+tibble(x = read_lines("../advent_of_code_2021/day_2.txt")) %>% 
+  separate(col = x, into = c("direction","distance"), convert = TRUE) %>% 
+  group_by(real_direction = direction == "forward") %>% 
+  mutate(distance = ifelse(direction == "up", distance * -1, distance)) %>% 
+  summarise(movement = sum(distance)) %>% 
+  pull(movement) %>% prod()
+
+
+
+
+tibble(x = read_lines("../advent_of_code_2021/day_2.txt")) %>% 
+  separate(col = x, into = c("direction","distance"), convert = TRUE) %>%
+  mutate(distance = as.double(distance)) %>% 
+  mutate(aim_change = case_when(direction == "forward" ~ 0,
+                                direction == "down" ~ distance,
+                                direction == "up" ~ -1 * distance)) %>% 
+  mutate(aim = cumsum(aim_change)) %>% 
+  mutate(horizontal_position_increase = ifelse(direction == "forward", distance, 0)) %>% 
+  mutate(depth_position_increase = ifelse(direction == "forward", aim * distance,0)) %>% 
+  mutate(horizontal_position = cumsum(horizontal_position_increase)) %>% 
+  mutate(depth_position = cumsum(depth_position_increase)) %>% 
+  slice_tail(n = 1) %>% 
+  summarise(answer = horizontal_position * depth_position) 
