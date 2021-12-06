6bb810eab7b6cfcecd23b6dfacfb35412b43b68a
dusty-turner <dusty.s.turner.mil@mail.mil>
2021-12-05 20:03:25 -0500

    part 2 day 5

--- 01_scripts/day_5.R
+++ 01_scripts/day_5.R
@@ -5,51 +5,90 @@ movements <-
 tibble(directions = read_lines("02_data/day_5.txt")) %>%
   mutate(directions = str_replace_all(string = directions, pattern = " -> ", ",")) %>% 
   separate(col = directions, into = c("x1", "y1", "x2", "y2"), sep = ",", convert = T) %>% 
-  select(x1,x2,y1,y2) %>% 
-  filter(x1==x2 | y1==y2) 
-  # mutate(minx1 = min(x1,x2)) %>% 
-  # group_split(id = row_number()) %>% 
-  # map(~select(.,-id)) 
-
-max <- movements %>% pivot_longer(cols = everything()) %>% summarise(max(value)) %>% pull()
+  select(x1,x2,y1,y2) 
 
 create_lines <- function(lines){
 
 move <- lines
 
-if(move$y1==move$y2){  
-xlow  <- min(move$x1, min(move$x2))
-xhigh <- max(move$x1, min(move$x2))
-ylow  <- min(move$y1, min(move$y2))
-yhigh <- max(move$y1, min(move$y2))
-byrow <- T
-} else if(move$x1==move$x2){
+if(move$y1==move$y2) {
+  xlow  <- min(move$x1, min(move$x2))
+  xhigh <- max(move$x1, min(move$x2))
+  ylow  <- min(move$y1, min(move$y2))
+  yhigh <- max(move$y1, min(move$y2))
+  byrow <- T
+} else if (move$x1 == move$x2) {
   ylow  <- min(move$x1, min(move$x2))
   yhigh <- max(move$x1, min(move$x2))
   xlow  <- min(move$y1, min(move$y2))
   xhigh <- max(move$y1, min(move$y2))
- byrow <- F
+  byrow <- F
 }
 
-  
 out <-
-matrix(
-c(
-rep(rep(0,10),ylow),
-
-c(rep(0,xlow),rep(1,xhigh-xlow+1), rep(0,9-xhigh)),
-
-rep(rep(0,10),9-ylow)
-), byrow = byrow, nrow = 10, ncol = 10)
+  matrix(
+    c(
+      rep(rep(0,1000),ylow), ## zeroes before row/col
+      c(rep(0,xlow),rep(1,xhigh-xlow+1), rep(0,999-xhigh)), ## row/col of 1s
+      rep(rep(0,1000),999-ylow) ## zeroes after row/col
+      ), byrow = byrow, nrow = 1000, ncol = 1000
+    )
 
 return(out)
 
 }
 
-movements %>% group_split(row_number(), .keep = F) %>% 
+movements %>% 
+  filter(x1==x2 | y1==y2) %>% 
+  group_split(row_number(), .keep = F) %>% 
   purrr::map(~create_lines(lines = .x)) %>% 
-  reduce(`+`) %>% as_tibble() %>% 
+  reduce(`+`) %>% 
+  as_tibble() %>% 
   pivot_longer(cols = everything()) %>% 
   filter(value >=2) %>% 
   count()
 
+###
+
+cross_routes <- function(move){
+
+  xlow  <- min(move$x1, min(move$x2))
+  xhigh <- max(move$x1, min(move$x2))
+  ylow  <- min(move$y1, min(move$y2))
+  yhigh <- max(move$y1, min(move$y2))
+
+mat <-  matrix(rep(0,1000000), ncol = 1000)
+
+if((move$x2>move$x1 & move$y2>move$y1) | (move$x2<move$x1 & move$y2<move$y1)){
+  for (i in 1:length(xlow:xhigh)) {
+    mat[ylow+i,xlow+i] <- 1
+  }
+} else{
+  for (i in 1:length(xlow:xhigh)) {
+    mat[ylow+i,xhigh+2-i] <- 1
+  }
+  
+}
+
+return(mat)
+
+}
+
+a <-
+  movements %>% 
+  filter(x1==x2 | y1==y2) %>% 
+  group_split(row_number(), .keep = F) %>% 
+  purrr::map(~create_lines(lines = .x)) %>% 
+  reduce(`+`) 
+
+b <-
+  movements %>%
+  filter(x1!=x2 & y1!=y2) %>%
+  group_split(row_number(), .keep = F) %>% 
+  purrr::map(~cross_routes(move = .x)) %>% 
+  reduce(`+`) 
+ 
+as_tibble(a+b) %>% 
+  pivot_longer(cols = everything()) %>%
+  filter(value >=2) %>%
+  count()
